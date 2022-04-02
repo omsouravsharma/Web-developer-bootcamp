e92ecc506bed9173c2dcf5d004edc111b7b20270
omsouravsharma <souravsharma.sep22@gmail.com>
2022-04-02 23:53:19 +0530

    JS-day2

--- javascript/test.js
+++ javascript/test.js
@@ -1,2 +1,16 @@
 let msg = "string"
 print(msg)
+
+console.log("Sourav")
+
+let rating = 2;
+
+if (rating === 3) {
+    console.log("Value is 3")
+}
+console.log("Sharma")
+
+let random = Math.random();
+if (random < 0.5) {
+    console.log(random)
+}
